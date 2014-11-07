a024461de5f67896cdf5d51c265ec7e85aa0a177
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-11-07 12:59:04 -0600

    Completed Exercise 2.1, a recursive function for the nth Fibonacci number

--- gettingstarted/src/main/scala/io/penland365/fpinscala/Boot.scala
+++ gettingstarted/src/main/scala/io/penland365/fpinscala/Boot.scala
@@ -3,16 +3,36 @@ package boot
 object Boot {
 
   def main(args: Array[String]): Unit = {
-    println("Hello, gettingstarted!")
-    val i = factorial(7)
-    println("Value of factorial was " + i)
+    println("This is Chapter 2, gettingstarted")
+    runExercise21
+  }
+
+  private def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(currMinus2: Int, currMinus1: Int, curr:Int, n: Int): Int = {
+      val fib = currMinus2 + currMinus1
+      if(curr == n ) fib
+      else loop(currMinus1, fib, curr + 1, n)
+    }
+
+    if( n < 0) return -1
+    if( n == 0) return 0
+    else if (n == 1) return 1
+    else loop(0, 1, 2, n)
   }
 
   private def factorial(n: Int): Int = {
+    @annotation.tailrec
     def loop(n: Int, acc: Int): Int =
       if(n <= 0) acc
       else loop(n-1, n*acc)
 
     loop(n, 1)
+  }
+
+  private def runExercise21(): Unit = {
+    println("Running Exercise 2.1, a recursive function for the nth Fibonacci number")
+    val i =  25 
+    println("fib(" + i + ") was --> " + fib(i))
   }
 }
