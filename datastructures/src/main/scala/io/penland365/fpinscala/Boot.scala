67565b951b7edee534f1ac6b30663e532ff32278
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-11-06 21:08:12 -0600

    First two folders added for a multi project build

---
+++ datastructures/src/main/scala/io/penland365/fpinscala/Boot.scala
@@ -0,0 +1,8 @@
+package boot
+
+object Boot {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, datastructures!")
+  }
+}
---
+++ project/Build.scala
@@ -0,0 +1,11 @@
+import sbt._
+import Keys._
+
+object FPInScala extends Build {
+
+   scalacOptions ++= Seq("-encoding", "utf8", "-unchecked", "-deprecation")
+
+   lazy val root = Project(id = "root", base = file(".")) aggregate(gettingstarted)
+   lazy val gettingstarted = Project(id = "gettingstarted", base = file("gettingstarted"))
+   lazy val datastructures = Project(id = "datastructures", base = file("datastructures"))
+}
