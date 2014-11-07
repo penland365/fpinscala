41845cddc0714ba2f9e1a12b32be408a53e2f575
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-11-07 09:18:41 -0600

    Added basic internal functional loop

--- gettingstarted/src/main/scala/io/penland365/fpinscala/Boot.scala
+++ gettingstarted/src/main/scala/io/penland365/fpinscala/Boot.scala
@@ -4,5 +4,15 @@ object Boot {
 
   def main(args: Array[String]): Unit = {
     println("Hello, gettingstarted!")
+    val i = factorial(7)
+    println("Value of factorial was " + i)
+  }
+
+  private def factorial(n: Int): Int = {
+    def loop(n: Int, acc: Int): Int =
+      if(n <= 0) acc
+      else loop(n-1, n*acc)
+
+    loop(n, 1)
   }
 }
