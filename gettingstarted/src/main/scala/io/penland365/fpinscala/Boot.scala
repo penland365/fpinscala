236d2184933440cc111d70d204dea3061357a8ab
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-11-07 16:10:17 -0600

    Completed exercise22

--- gettingstarted/src/main/scala/io/penland365/fpinscala/Boot.scala
+++ gettingstarted/src/main/scala/io/penland365/fpinscala/Boot.scala
@@ -2,9 +2,11 @@ package boot
 
 object Boot {
 
+  type exercise = () => Unit
+
   def main(args: Array[String]): Unit = {
     println("This is Chapter 2, gettingstarted")
-    runExercise21
+    runExercise("Exercise 2.2", exercise22)
   }
 
   private def fib(n: Int): Int = {
@@ -30,9 +32,46 @@ object Boot {
     loop(n, 1)
   }
 
-  private def runExercise21(): Unit = {
-    println("Running Exercise 2.1, a recursive function for the nth Fibonacci number")
-    val i =  25 
+  private def isSorted[A](arr: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(i: Int, valid: Boolean): Boolean = {
+      if(!valid || i >= arr.length) valid
+      else if(i == 0) loop(i + 1, valid)
+      else loop(i + 1, ordered(arr(i-1), arr(i)))
+    }
+    loop(0, true)
+  }
+
+  private def runExercise(fnName: String, f: exercise): Unit = {
+    println(fnName)
+    f()
+  }
+
+  private def exercise21(): Unit = {
+    val i =  25
     println("fib(" + i + ") was --> " + fib(i))
   }
+
+  private def exercise22(): Unit = {
+    val iOrdered = new Function2[Int, Int, Boolean] {
+      def apply(a: Int, b: Int) = a < b
+    }
+    val iArr = Array(1,2,3,4,5)
+    println("Array[Int] sorted (should be true) --> " + isSorted(iArr, iOrdered))
+
+    val sOrdered = new Function2[String, String, Boolean] {
+      def apply(a: String, b: String) = a < b
+    }
+    val sArr = Array("a","k","c")
+    println("Array[String] sorted (should be false) --> " + isSorted(sArr, sOrdered))
+  }
+
+
+
+
+
+
+
+
+
 }
