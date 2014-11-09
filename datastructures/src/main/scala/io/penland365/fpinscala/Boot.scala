09994672dcf23fa07055db5e199b22730ccb7210
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-11-09 16:14:06 -0600

    Completed exercise 3.1

--- datastructures/src/main/scala/io/penland365/fpinscala/Boot.scala
+++ datastructures/src/main/scala/io/penland365/fpinscala/Boot.scala
@@ -2,7 +2,25 @@ package boot
 
 object Boot {
 
+  type exercise = () => Unit
+
   def main(args: Array[String]): Unit = {
-    println("Hello, datastructures!")
+    println("This is Chapter 3, datastructures")
+    runExercise("Exercise 3.1", exercise31)
+  }
+
+  private def exercise31(): Unit = {
+    val x = List(1,2,3,4,5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case _ => 101
+    }
+    println(x)
+  }
+
+  private def runExercise(fnName: String, f: exercise): Unit = {
+    println(fnName)
+    f()
   }
 }
