0878a9195db188321207506c4c8aff4744835b3c
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-11-17 11:46:19 -0600

    Completed exercise 4.2

--- errorhandling/src/main/scala/io/penland365/fpinscala/Boot.scala
+++ errorhandling/src/main/scala/io/penland365/fpinscala/Boot.scala
@@ -6,8 +6,15 @@ object Boot {
 
   def main(args: Array[String]): Unit = {
     println("This is Chapter 3, errorhandling")
-    runExercise("Exercise 4.1", exercise41)
+    runExercise("Exercise 4.2", exercise42)
   }
+
+  private def mean(xs: Seq[Double]): Option[Double] =
+    if(xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  private def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap(m => mean(xs.map( x => math.pow(x - m, 2))))
 
   private def exercise41(): Unit = {
     val o = Some(71)
@@ -20,6 +27,12 @@ object Boot {
     println("o.flatMap(x => Some(x + 1)) = " + o.flatMap(x => Some(x + 1)))
     println("o.getOrElse(17) = " + o.getOrElse(17))
     println("n.getOrElse(17) = " + n.getOrElse(17))
+  }
+
+  private def exercise42(): Unit = {
+    val l = Seq(1.0,2.0,3.0,4.0,5.0)
+    println("l = " + l)
+    println("variance(l) = " + variance(l))
   }
 
   private def runExercise(fName: String, f: exercise): Unit = {
