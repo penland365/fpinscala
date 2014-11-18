4bbedf2b0ae68bc8320a16bf73c8aa1f3dfc7e2c
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-11-18 10:08:02 -0600

    Completed exercises 4.3, 4.4, 4.5

--- errorhandling/src/main/scala/io/penland365/fpinscala/Boot.scala
+++ errorhandling/src/main/scala/io/penland365/fpinscala/Boot.scala
@@ -6,7 +6,7 @@ object Boot {
 
   def main(args: Array[String]): Unit = {
     println("This is Chapter 3, errorhandling")
-    runExercise("Exercise 4.2", exercise42)
+    runExercise("Exercise 4.4", exercise44)
   }
 
   private def mean(xs: Seq[Double]): Option[Double] =
@@ -15,6 +15,9 @@ object Boot {
 
   private def variance(xs: Seq[Double]): Option[Double] =
     mean(xs).flatMap(m => mean(xs.map( x => math.pow(x - m, 2))))
+
+  private def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C): Option[C] = 
+    a.flatMap(aa => b map(bb => f(aa, bb)))
 
   private def exercise41(): Unit = {
     val o = Some(71)
@@ -33,6 +36,15 @@ object Boot {
     val l = Seq(1.0,2.0,3.0,4.0,5.0)
     println("l = " + l)
     println("variance(l) = " + variance(l))
+  }
+
+  private def exercise44(): Unit = {
+    val x = List(Some(1), Some(2), Some(3))
+    val xs = List(Some(1), Some(2), None)
+    println("x = " + x)
+    println("Option.sequence(x) = " + Option.sequence(x))
+    println("xs = " + xs)
+    println("Option.sequence(xs) = " + Option.sequence(xs))
   }
 
   private def runExercise(fName: String, f: exercise): Unit = {
--- errorhandling/src/main/scala/io/penland365/fpinscala/Option.scala
+++ errorhandling/src/main/scala/io/penland365/fpinscala/Option.scala
@@ -27,3 +27,28 @@ sealed trait Option[+A] {
 
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if(xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap(m => mean(xs.map( x => math.pow(x - m, 2))))
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C): Option[C] = 
+    a.flatMap(aa => b map(bb => f(aa, bb)))
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h.flatMap(hh => sequence(t) map (hh :: _))
+  }
+
+  def traverse[A,B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
+  }
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] = 
+    traverse(a)(hh => hh)
+}
