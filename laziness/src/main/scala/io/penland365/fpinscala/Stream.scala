cacfb848c1eb87b5b1a29a83967c4236402664db
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-11-19 17:06:14 -0600

    Completed exercise 5.1

---
+++ laziness/src/main/scala/io/penland365/fpinscala/Stream.scala
@@ -0,0 +1,35 @@
+package boot
+
+sealed trait Stream[+A] {
+
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+
+  def toList: List[A] = {
+    val buf = new collection.mutable.ListBuffer[A]
+    @annotation.tailrec
+    def loop(s: Stream[A]): List[A] = s match {
+      case Cons(h, t) => buf += h(); loop(t())
+      case _ => buf.toList
+    }
+
+    loop(this)
+  }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, t1: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = t1
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if(as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
