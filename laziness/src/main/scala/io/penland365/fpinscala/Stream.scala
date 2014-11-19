7df4b2fdda02857ee6203f97cab45a979cee36da
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-11-19 17:56:39 -0600

    Completed exercise 5.2

--- laziness/src/main/scala/io/penland365/fpinscala/Stream.scala
+++ laziness/src/main/scala/io/penland365/fpinscala/Stream.scala
@@ -17,6 +17,29 @@ sealed trait Stream[+A] {
 
     loop(this)
   }
+
+  def take(n: Int): Stream[A] = {
+    if(n > 0) this match {
+      case Cons(h, t) if n == 1 => Stream.cons(h(), Stream.empty)
+      case Cons(h, t) => Stream.cons(h(), t().take(n-1))
+      case _ => Stream.empty
+
+    }
+    else Stream()
+  }
+
+  def drop(n: Int): Stream[A] = {
+    @annotation.tailrec
+    def loop(i: Int, s: Stream[A]): Stream[A] = {
+      if(i <= 0) s
+      else s match {
+        case Cons(h,t) => loop(i-1, t())
+        case _ => Stream()
+      }
+    }
+
+    loop(n, this)
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
