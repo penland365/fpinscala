ba7b72bdc95569f17d4c3146827f8ca963e7b136
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-12-25 12:58:25 -0600

    Completed exercises 6.1, 6.2, 6.3, 6.4

--- state/src/main/scala/fpinscala/state/Boot.scala
+++ state/src/main/scala/fpinscala/state/Boot.scala
@@ -6,13 +6,33 @@ object Boot {
 
   def main(args: Array[String]): Unit = {
     println("This is Chapter 6, Purely functional state")
-    runExercise("Exercise 6.1", exercise61)
+    runExercise("Exercise 6.4", exercise64)
   }
 
   private def exercise61(): Unit = {
-    val rng = SimpleRNG(42)
-    val(n1, rng2) = rng.nextInt
+    val(n1, rng2) = RNG.nonNegativeInt(SimpleRNG(71))
     println(n1)
+  }
+
+  private def exercise62(): Unit = {
+    val t = RNG.double(SimpleRNG(19))
+    println(t._1)
+  }
+
+  private def exercise63(): Unit = {
+    val i = RNG.intDouble(SimpleRNG(11))
+    println("intDouble => (" + i._1._1 + ", " + i._1._2 + ")")
+
+    val j = RNG.doubleInt(SimpleRNG(13))
+    println("doubleInt=> (" + j._1._1 + ", " + j._1._2 + ")")
+
+    val k = RNG.double3(SimpleRNG(17))
+    println("double3=> (" + k._1._1 + ", " + k._1._2 + ", " + k._1._3 + ")")
+  }
+
+  private def exercise64(): Unit = {
+    val(l, rng) = RNG.ints(7)(SimpleRNG(23))
+    l.foreach(x => println(x))
   }
 
   private def runExercise(fnName: String, f: exercise): Unit = {
--- state/src/main/scala/fpinscala/state/RNG.scala
+++ state/src/main/scala/fpinscala/state/RNG.scala
@@ -4,6 +4,51 @@ trait RNG {
   def nextInt: (Int, RNG)
 }
 
+object RNG {
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val i = rng.nextInt
+    val j = if(i._1 == Int.MinValue) 0 else i._1.abs
+    (j, i._2)
+  }
+
+  def double(rng: RNG): (Double, RNG) = {
+    val(i, nextRng) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1.0), nextRng)
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val i = nonNegativeInt(rng)
+    val j = double(i._2)
+    ((i._1, j._1), j._2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val i = double(rng)
+    val j = nonNegativeInt(i._2)
+    ((i._1, j._1), j._2)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val i = double(rng)
+    val j = double(i._2)
+    val k = double(j._2)
+    ((i._1, j._1, k._1), k._2)
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    @annotation.tailrec
+    def go(n: Int, l: List[Int], r: RNG): (List[Int], RNG) = {
+      if(n > 0) {
+        val(i, nextRng) = nonNegativeInt(r)
+        go(n - 1, i :: l, nextRng)
+      }
+      else (l, r)
+    }
+    go(count, Nil, rng)
+  }
+}
+
 case class SimpleRNG(seed: Long) extends RNG {
   def nextInt: (Int, RNG) = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
